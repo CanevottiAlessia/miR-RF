8a5de9efea2a2867a3080e7490e2b6c4e831ab94
CanevottiAlessia <153506270+CanevottiAlessia@users.noreply.github.com>
2024-02-19 10:30:12 +0100

    Update make_miR_pred.R

--- make_miR_pred.R
+++ make_miR_pred.R
@@ -1,14 +1,15 @@
 #!/usr/bin/env Rscript
 
-# Check if the trained model exists, if not, train and save it
-if (!file.exists("trained_model_new.RDS")) {
-  train_model_and_save()
-}
 
 args=commandArgs(trailingOnly=TRUE)
 name_table=args[1]
 name_final=args[2]
 trained_model <- readRDS("trained_model_new.RDS")
+
+# Check if the trained model exists, if not, train and save it
+if (!file.exists("trained_model_new.RDS")) {
+  train_model_and_save()
+}
 
 # PREDICTION FUNCTION
 predict_output <- function(trained_model, new_data) {
@@ -27,12 +28,25 @@ input_R[is.na(input_R)] <- 0
 column_index <- which(names(input_R) == "real miRNA")
 new_column_order <- c(names(input_R)[-column_index], "real miRNA")
 df_1917 <-input_R[new_column_order]
-new_1917_norm_by_len = df_1917[,1:124]/df_1917$`hairpin length`
-new_1917_normL <- cbind(new_1917_norm_by_len, df_1917[, 125:125])
-colnames(new_1917_normL)[125] <- 'real miRNA'
-set.seed(825)
+
+# Normalization
+performNormalization <- function(data) {
+  normalized_data <- data[, 1:36] / data$`hairpin length`
+  normalized_data[, 37] <- data[, 37] / 100
+  normalized_data <- cbind(normalized_data, data[, 38:71] / data$`hairpin length`)
+  normalized_data <- cbind(normalized_data, data[, 72:81])
+  normalized_data <- cbind(normalized_data, data[, 82:124] / data$`hairpin length`)
+  normalized_data <- cbind(normalized_data, data[, 125:125])
+  colnames(normalized_data)[37] <- 'hairpin length'
+  colnames(normalized_data)[125] <- 'real miRNA'
+  return(normalized_data)
+}
+
+new_1917_normL <- performNormalization(df_1917)
+
 
 # Use the loaded trained model to make predictions
+set.seed(825)
 output <- predict_output(trained_model, new_1917_normL)
 colnames(output) <- c("miRNA name", "prediction")
 
