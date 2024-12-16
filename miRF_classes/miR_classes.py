d24babcd5df88e171d8371a5e364ec76731fa26b
CanevottiAlessia <153506270+CanevottiAlessia@users.noreply.github.com>
2024-12-16 16:00:22 +0100

    Update miR_classes.py

--- miRF_classes/miR_classes.py
+++ miRF_classes/miR_classes.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 
 def remove_temp_files():
     """Rimuove tutti i file che contengono 'temp' o 'final_hairpins.' nel nome nella directory corrente."""
@@ -11,7 +12,7 @@ def remove_temp_files():
             except Exception as e:
                 print(f"Error removing file {file_name}: {e}")
 
-def runner(rnafold, fasta, base_pred):
+def runner(rnafold, fasta, base_pred, out):
     try:
         os.system("python3 3_get_fasta_for_insertion.py " + rnafold)
     except:
@@ -56,7 +57,7 @@ def runner(rnafold, fasta, base_pred):
     intermediate_file7 = base_pred + "_temp_classes_table.tsv_temp_IS_class.tsv"
     intermediate_file8 = base_pred + "_temp_classes_table.tsv_temp_classes_mirnas.txt"
     try:
-        os.system("python3 9_make_final_classes.py " + intermediate_file6 + " " + intermediate_file7 + " " + intermediate_file8 + " " + rnafold)
+        os.system("python3 9_make_final_classes.py " + intermediate_file6 + " " + intermediate_file7 + " " + intermediate_file8 + " " + out)
     except:
         print("Error: Failed to execute python3 9_make_final_classes.py")
         pass
@@ -65,7 +66,21 @@ def runner(rnafold, fasta, base_pred):
     remove_temp_files()
 
 if __name__ == "__main__":
-    rnafold = sys.argv[1]
-    fasta = sys.argv[2]
-    base_pred = sys.argv[3]
-    runner(rnafold, fasta, base_pred)
+    parser = argparse.ArgumentParser(description="make miRF classes helper")
+    parser.add_argument("rnafold", type=str, help="RNAfold file name")
+    parser.add_argument("fasta", type=str, help="FASTA file name")
+    parser.add_argument("base_pred", type=str, help="Annotation file name")
+    parser.add_argument("out", type=str, help="Output file name - you can choose a name for the output file")
+
+    # Parsing degli argomenti
+    args = parser.parse_args()
+
+    # Controllo se il file di output esiste già
+    if os.path.exists(args.out):
+        response = input(f"Warning: The output file '{args.out}' already exists. Do you want to overwrite it? (y/n): ").strip().lower()
+        if response != 'y':
+            print("Operation cancelled by the user.")
+            sys.exit(1)
+
+    # Passaggio degli argomenti alla funzione runner
+    runner(args.rnafold, args.fasta, args.base_pred, args.out)
