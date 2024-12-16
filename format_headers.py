73cc32259e5b0d1f83738ddc23d2460a8eac3ae6
CanevottiAlessia <153506270+CanevottiAlessia@users.noreply.github.com>
2024-12-16 08:06:23 +0100

    Update and rename Empty_file.txt to format_headers.py

---
+++ format_headers.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+if __name__=="__main__":
+   user_file = sys.argv[1]
+   user_file2 = sys.argv[2]
+
+def replace_spaces_in_headers(file_path, output_path):
+    with open(file_path, 'r') as infile, open(output_path, 'w') as outfile:
+        for line in infile:
+            if line.startswith('>'):
+                # Replace spaces and '\t' with '_' in header lines
+                line = line.replace(' ', '_').replace('\t', '_')
+            outfile.write(line)
+
+# Use
+input_file = 'hg19_hsa_hairpin.fa'
+output_file = 'formatted_hg19_hsa_hairpin.fa'
+replace_spaces_in_headers(input_file, output_file)
