80dacc1e07edd26efc19794c0993cadbd8ffc6d1
CanevottiAlessia <153506270+CanevottiAlessia@users.noreply.github.com>
2024-12-23 10:22:18 +0100

    Update miR_classes.py

--- miRF_classes/miR_classes.py
+++ miRF_classes/miR_classes.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 def remove_temp_files():
-    """Rimuove tutti i file che contengono 'temp' o 'final_hairpins.' nel nome nella directory corrente."""
+    """Removes all files with 'temp' or 'final_hairpins.' in current directory."""
     for file_name in os.listdir("."):
         if "temp" in file_name or "final_hairpins." in file_name:
             try:
@@ -20,9 +20,9 @@ def runner(rnafold, fasta, base_pred, out):
         pass
     intermediate_file = rnafold + "_final_hairpins.txt"
     try:
-        os.system("python3 4_SNP_insertion.py " + fasta + " " + intermediate_file)
+        os.system("python3 SNP_insertion.py " + fasta + " " + intermediate_file)
     except:
-        print("Error: Failed to execute python3 4_SNP_insertion.py")
+        print("Error: Failed to execute python3 SNP_insertion.py")
         pass
     intermediate_file2 = intermediate_file + "_potential_RNAfold.txt"
     try:
@@ -32,37 +32,37 @@ def runner(rnafold, fasta, base_pred, out):
         pass
     intermediate_file3 = "features_table_for_" + intermediate_file2 + "_temp_pred_potential.tsv"
     try:
-        os.system("python3 5_merge_table.py " + base_pred + " " + intermediate_file3)
+        os.system("python3 merge_table.py " + base_pred + " " + intermediate_file3)
     except:
-        print("Error: Failed to execute python3 5_merge_table.py")
+        print("Error: Failed to execute python3 merge_table.py")
         pass
     intermediate_file4 = base_pred + "_temp_classes_table.tsv"
     try:
-        os.system("python3 6_get_lens.py " + rnafold)
+        os.system("python3 get_lens.py " + rnafold)
     except OSError as e:
-        print("Error: Failed to execute python3 6_get_lens.py")
+        print("Error: Failed to execute python3 get_lens.py")
         pass
     intermediate_file5 = rnafold + "_temp_len_hairpins.txt"
     try:
-        os.system("python3 7_make_fisher_test.py " + intermediate_file4 + " " + intermediate_file5)
+        os.system("python3 make_fisher_test.py " + intermediate_file4 + " " + intermediate_file5)
     except:
-        print("Error: Failed to execute python3 7_make_fisher_test.py")
+        print("Error: Failed to execute python3 make_fisher_test.py")
         pass
     try:
-        os.system("python3 8_make_first_classes.py " + intermediate_file4)
+        os.system("python3 make_first_classes.py " + intermediate_file4)
     except:
-        print("Error: Failed to execute python3 8_make_temp_classes.py")
+        print("Error: Failed to execute python3 make_temp_classes.py")
         pass
     intermediate_file6 = base_pred + "_temp_classes_table.tsv_temp_DR_class.tsv"
     intermediate_file7 = base_pred + "_temp_classes_table.tsv_temp_IS_class.tsv"
     intermediate_file8 = base_pred + "_temp_classes_table.tsv_temp_classes_mirnas.txt"
     try:
-        os.system("python3 9_make_final_classes.py " + intermediate_file6 + " " + intermediate_file7 + " " + intermediate_file8 + " " + out)
+        os.system("python3 make_final_classes.py " + intermediate_file6 + " " + intermediate_file7 + " " + intermediate_file8 + " " + out)
     except:
-        print("Error: Failed to execute python3 9_make_final_classes.py")
+        print("Error: Failed to execute python3 make_final_classes.py")
         pass
 
-    # Rimuove tutti i file temporanei alla fine
+    # Removes temp files
     remove_temp_files()
 
 if __name__ == "__main__":
@@ -72,15 +72,15 @@ if __name__ == "__main__":
     parser.add_argument("base_pred", type=str, help="Annotation file name")
     parser.add_argument("out", type=str, help="Output file name - you can choose a name for the output file")
 
-    # Parsing degli argomenti
+    # Parsing arguments
     args = parser.parse_args()
 
-    # Controllo se il file di output esiste già
+    # Check if the output file already exists
     if os.path.exists(args.out):
         response = input(f"Warning: The output file '{args.out}' already exists. Do you want to overwrite it? (y/n): ").strip().lower()
         if response != 'y':
             print("Operation cancelled by the user.")
             sys.exit(1)
 
-    # Passaggio degli argomenti alla funzione runner
+    # Passing argvs to the runner function
     runner(args.rnafold, args.fasta, args.base_pred, args.out)
